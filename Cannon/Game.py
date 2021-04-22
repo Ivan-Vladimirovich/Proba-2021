dad8788f02178eb21850a30e9020f17dc7a2f6f4
Ivan Vladimirovich <iiv88@mail.ru>
2021-04-22 21:08:47 +0700

    Добавил картинки с фоном. немного изменил взаимодействие спрайтов

--- Cannon/Game.py
+++ Cannon/Game.py
@@ -1,20 +1,32 @@
 import pygame
-from pygame.draw import *
 import numpy as np
 from random import randint
+from os import path
 
+img_dir = path.join(path.dirname(__file__), 'C:\\Users\\ВАНЯ\\Proba-2021\\img')
 
 pygame.init()
 # задание размера экрана
 xsc = 600
 ysc = 600
-FPS = 30
+FPS = 35
 SCREEN_SIZE = [xsc, ysc]
 screen = pygame.display.set_mode((xsc, ysc))
 pygame.mixer.init()
 pygame.display.set_caption('Cannon')
-# заливка экрана цветом
-screen.fill((76, 84, 71))
+
+
+background = pygame.image.load(path.join(img_dir, "C:\\Users\\ВАНЯ\\Proba-2021\\img\\back.png")).convert()
+background_rect = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir, "C:\\Users\\ВАНЯ\\Proba-2021\\img\\playerShip1_orange.png")).convert()
+bad_img = pygame.image.load(path.join(img_dir, "C:\\Users\\ВАНЯ\\Proba-2021\\img\\playerShip1_blue.png")).convert()
+bullet_img = pygame.image.load(path.join(img_dir, "C:\\Users\\ВАНЯ\\Proba-2021\\img\\Lasers\\laserGreen10.png")).convert()
+bomb_img = pygame.image.load(path.join(img_dir, "C:\\Users\\ВАНЯ\\Proba-2021\\img\\Lasers\\laserBlue12.png")).convert()
+# Рендеринг
+screen.fill((0,  0, 0))
+screen.blit(background, background_rect)
+
+
 clock = pygame.time.Clock()
 finished = False
 
@@ -26,9 +38,11 @@ def rand_color():
 class Cannon(pygame.sprite.Sprite):
     def __init__(self, xg, yg, angle=0):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((60, 40))
+        self.image = player_img
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
-        self.image.fill((83, 114, 63))
+        self.radius = 20
         self.rect.bottom = yg*0.95
         self.rect.centerx = xg/2
         self.healf = 5
@@ -45,12 +59,6 @@ class Cannon(pygame.sprite.Sprite):
             self.speedx = 8
         self.rect.x += self.speedx
 
-    def set_angle(self, target_pos):
-        """
-        Sets gun's direction to target position.
-        """
-        self.angle = np.arctan2(target_pos[1] - self.rect.bottom, target_pos[0] - self.rect.centerx)
-
     def strike(self):
 
         bullet = Shell(self.rect.centerx, self.rect.top)
@@ -61,12 +69,12 @@ class Cannon(pygame.sprite.Sprite):
 class Shell(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 20))
-        self.image.fill(rand_color())
+        self.image = bullet_img
+        self.image = pygame.transform.scale(bullet_img, (10, 20))
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
-        self.speedy = -30
+        self.speedy = -25
 
     def update(self):
 
@@ -78,15 +86,16 @@ class Shell(pygame.sprite.Sprite):
 class Target(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((20, 20))
-        self.image.fill(rand_color())
+        self.image = pygame.transform.scale(pygame.transform.rotate(bad_img, 180), (50, 35))
+        self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
-        self.rect.x = randint(20, 580)
-        self.rect.y = randint(20, 480)
-        self.speedy = randint(5, 15)
+        self.radius = int(self.rect.width * .85 / 2)
+        self.rect.centerx = randint(20, 580)
+        self.rect.bottom = randint(-50, 0)
+        self.speedy = randint(1, 12)
         self.healf = 4
-        self.flag_x = 0
-        self.flag_y = 0
+        self.flag_x = randint(0, 2)
+        self.flag_y = randint(0, 2)
 
     def update(self):
         if self.flag_x == 0:
@@ -104,20 +113,45 @@ class Target(pygame.sprite.Sprite):
         elif self.rect.x < 15:
             self.flag_x = 0
             self.speedy = randint(5, 15)
-        if self.rect.y > ysc  - 15:
+        if self.rect.y > 0.75*ysc - 15:
             self.flag_y = 0
             self.speedy = randint(5, 15)
         elif self.rect.y < 15:
             self.flag_y = 1
             self.speedy = randint(5, 15)
 
+        k = randint(0, 1000)
+        if k < 10:
+            bomb = Bomb(self.rect.x, self.rect.y)
+            all_sprites.add(bomb)
+            bombs.add(bomb)
+
+
+class Bomb(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(bomb_img, (8, 15))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = randint(5, 10)
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom > ysc:
+            self.kill()
+
 
 # Цикл игры
 all_sprites = pygame.sprite.Group()
+background = pygame.image.load(path.join(img_dir, "C:\\Users\\ВАНЯ\\Proba-2021\\img\\back.png")).convert()
+background_rect = background.get_rect()
+all_sprites.draw(screen)
 gun = Cannon(xsc, ysc)
 all_sprites.add(gun)
 mobs = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
+bombs = pygame.sprite.Group()
 for i in range(6):
     m = Target()
     all_sprites.add(m)
@@ -127,7 +161,6 @@ runGame = True
 while runGame:
     clock.tick(FPS)
 
-    pygame.display.update()
     # Отслеживание событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -139,13 +172,15 @@ while runGame:
                 gun.speedx = -8
             if event.key == pygame.K_d:
                 gun.speedx = 8
-        if pygame.mouse.get_focused():
-            mouse_pos = pygame.mouse.get_pos()
-            gun.set_angle(mouse_pos)
 
     all_sprites.update()
-    # Проверка, не ударил ли моб игрока
-    hits = pygame.sprite.spritecollide(gun, mobs, False)
+    screen.fill((0, 0, 0))
+    screen.blit(background, background_rect)
+    all_sprites.draw(screen)
+    pygame.display.update()
+
+    # Проверка, не ударил ли моб игрокa
+    hits = pygame.sprite.spritecollide(gun, bombs, False)
     if hits:
         runGame = False
     # Проверка столкновения пули с мишенью
@@ -154,9 +189,7 @@ while runGame:
         m = Target()
         all_sprites.add(m)
         mobs.add(m)
-    # Рендеринг
-    screen.fill((76, 84, 71))
-    all_sprites.draw(screen)
+
     # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
